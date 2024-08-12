d3869d2f240a03d9b6a187a18ec183abd717545d
Ameya Taneja <ameyataneja1302@gmail.com>
2024-08-12 15:58:35 +0530

    Added evenHandler and getAllfiles module

---
+++ handlers/eventHandler.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const getAllFiles = require('../utils/getAllFiles');
+
+module.exports = (client) => {
+    const eventFolders  = getAllFiles(path.join(__dirname, ".." , "events", true))
+    for(const eventFolder of eventFolders){
+        let eventName;
+        eventName = eventFolder.replace(/\\/g, "/").split('/').pop();
+        eventName === "validations" ? (eventName = "interactionsCreate") : eventName;
+
+        client.on(eventName, async(arg) =>{
+            for(const eventFile of eventFiles){
+                const eventFunction = require(eventFile);
+                await eventFunction(client, arg);
+                           
+             }
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+const {Client, GatewayIntentBits} = require('discord.js')
+const eventHandler = require("");
+const client = new Client ({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+})
+
+eventHandler(client)
+client.login(process.env.TOKEN)
